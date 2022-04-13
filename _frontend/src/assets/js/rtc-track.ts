a054bada604351dfbacd1666f05c8a2bba97eb7c
jenesius <lokargenia@gmail.com>
2022-04-14 02:37:06 +0300

    Convert react to vue, update interface design

--- _frontend/src/assets/js/rtc-track.ts
+++
@@ -1,9 +0,0 @@
-export type StreamTrackType = 'screen-video' | 'user-audio' | 'user-video';
-export default class RTCTrack extends MediaStreamTrack {
-	readonly localType: StreamTrackType
-	
-	static wrap(track: MediaStreamTrack, type: StreamTrackType): RTCTrack {
-		return Object.defineProperties(track, {localType: {value: type}}) as RTCTrack;
-	}
-	
-}
